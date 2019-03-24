634a1e9bc22f25ac4fe0fdc9c14bac37a640cf7b
Sanja P <sanja.popovic@stud.unibas.ch>
2019-03-24 11:34:53 +0100

    Rewrote Javadoc and deleted unnecessary import and declaration in class PingManager

--- Buddler-Joe/src/main/java/net/playerhandling/PingManager.java
+++ Buddler-Joe/src/main/java/net/playerhandling/PingManager.java
@@ -2,28 +2,31 @@ package net.playerhandling;
 
 import net.packets.pingpong.PacketPing;
 
-import java.text.NumberFormat;
+
 import java.util.ArrayList;
 
 import static java.lang.Thread.sleep;
 
+/**
+ * The function of the PingManager is to automatically send pings to client and server and to calculate the respective average ping. This class is activated by the <code>ClientThread</code> and the <code>ClientLogic</code> class.
+ *
+ * @see ClientThread
+ * @see net.ClientLogic
+ */
+
 public class PingManager implements Runnable{
 
     /**
-     * every client and server create an instance of this class for themselves
-     * listOfPingTS saves all timestamps(sendingtime)
-     * in the variable ping is the average ping time
-     * data is timestamp in String
+     * @param listOfPingTS this list contains the creation time of all sent pings.
+     * @param ping the average ping
+     * @param clientId the identity of the client
      */
-    //TS=timestamps number in string
     private ArrayList<String> listOfPingTS;
-    //Arraylist mit time
     private float ping;
-    Thread thread;
     private int clientId;
 
     /**
-     * sending ping from server to client
+     * Creates a <code>PingManager</code> object when sending ping from server to client. The client is determined by the <code>clientId</code>.
      */
     public PingManager(int clientId) {
         listOfPingTS = new ArrayList<>();
@@ -32,12 +35,22 @@ public class PingManager implements Runnable{
     }
 
     /**
-     * sending ping from client to server
+     *Creates a <code>PingManager</code> object when sending ping from client to server.
      */
     public PingManager() {
         listOfPingTS = new ArrayList<>();
         ping = 0;
     }
+
+    /**
+     * Executes every second the automized sending of the pings and saves the creation time in <code>listOfPingTS</code>.
+     * The destination is determined by the <code>clientId</code>.
+     * If the clientId was passed, the ping would be sent to the client. Otherwise, the clientId will have the default value 0 and the ping will be sent to the server.
+     * A ping object will be created by instantiating the <code>PacketPing</code> class.
+     *
+     * @throws InterruptedException when thread is interrupted.
+     * @see PacketPing
+     */
 
     public void run() {
         while(true) {
@@ -63,14 +76,30 @@ public class PingManager implements Runnable{
         }
     }
 
+    /**
+     * Appends the creation time of the <code>PacketPing</code> object to <code>listOfPingTS</code>.
+     *
+     * @param timestamp creation time of the ping
+     */
 
     private void append(String timestamp) {
         listOfPingTS.add(timestamp);
     }
 
+    /**
+     * Deletes the creation time of the <code>PacketPing</code> object in <code>listOfPingTS</code> if the respective <code>PacketPong</code> object returned successfully.
+     *
+     * @param timestamp creation time of the ping
+     */
     public void delete(String timestamp) {
         listOfPingTS.remove(timestamp);
     }
+
+    /**
+     * Updates the average <code>ping</code>.
+     *
+     * @param diffTime the difference between the arrival time of the final <code>PacketPong</code> object and the creation time of the <code>PacketPing</code> object
+     */
 
     public void updatePing(long diffTime) {
         ping = (ping*9 + diffTime)/10f;
@@ -80,6 +109,11 @@ public class PingManager implements Runnable{
         return listOfPingTS;
     }
 
+    /**
+     * Informs every player about their ping.
+     *
+     * @return average ping time.
+     */
     public long getPing() {
         return (int) (ping);
     }
