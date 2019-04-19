ddde96ddad7abe465b05900f4cafe0d9e6d9a9c4
Sanja P <sanja.popovic@stud.unibas.ch>
2019-04-19 13:49:12 +0200

    deleted unnecessary lines

--- src/main/java/gui/lifeStatus/LifeStatus.java
+++ src/main/java/gui/lifeStatus/LifeStatus.java
@@ -1,34 +1,24 @@
 package gui.lifeStatus;
 
-import engine.io.InputHandler;
 import engine.render.Loader;
-import engine.render.fontmeshcreator.FontType;
-import engine.render.fontrendering.TextMaster;
-import game.Game;
 import gui.GuiTexture;
-import net.packets.chat.PacketChatMessageToServer;
 import org.joml.Vector2f;
-import org.joml.Vector3f;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 public class LifeStatus {
-
 
 
     private GuiTexture hearts;
 
 
     /**
-     * Initialize Chat, only needs to be called once on game init.
+     * 
      *
      * @param loader main loader
      */
     public LifeStatus(Loader loader) {
 
-        // Load the background image of the chat and set rendering parameters
+        // Loads the heart to screen and set rendering parameters
         hearts = new GuiTexture(
                         loader.loadTexture("heart"),
                         new Vector2f(-.95f, .87f),
