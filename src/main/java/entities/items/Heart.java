602b5ccca4d34dfaa13899953ea2e5694e066bbe
Viktor Urban <v.gsteiger@gmail.com>
2019-04-24 10:19:27 +0200

    Implemented Items

--- src/main/java/entities/items/Heart.java
+++ src/main/java/entities/items/Heart.java
@@ -36,7 +36,7 @@ public class Heart extends Item {
    * @param loader the loader to be passed on to this method.
    */
   public static void init(Loader loader) {
-    RawModel rawHeart = loader.loadToVao(ObjFileLoader.loadObj("block"));
+    RawModel rawHeart = loader.loadToVao(ObjFileLoader.loadObj("heart"));
     setPreloadedModel(new TexturedModel(rawHeart, new ModelTexture(loader.loadTexture("red"))));
   }
 
