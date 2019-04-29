b00b283f0c5a071c4433fb7ec36e75007742436e
Viktor Urban <v.gsteiger@gmail.com>
2019-04-29 14:51:52 +0200

    Fixed some issues with Unit Tests and updated gantt

--- src/test/java/net/lobbyhandling/TestServerLobbyList.java
+++ src/test/java/net/lobbyhandling/TestServerLobbyList.java
@@ -1,6 +1,7 @@
 package net.lobbyhandling;
 
 import java.util.concurrent.ConcurrentHashMap;
+
 import org.junit.Assert;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -114,30 +115,20 @@ public class TestServerLobbyList {
 
   @Test
   public void checkGetTopTen() {
-    Lobby testLobby1 = new Lobby("test1", 1, "mid");
-    Lobby testLobby2 = new Lobby("test2", 2, "large");
-    Lobby testLobby3 = new Lobby("test3", 3, "large");
-    Lobby testLobby4 = new Lobby("test4", 4, "large");
-    Lobby testLobby5 = new Lobby("test5", 5, "large");
-    Lobby testLobby6 = new Lobby("test6", 26, "large");
-    Lobby testLobby7 = new Lobby("test7", 7, "large");
-    Lobby testLobby8 = new Lobby("test8", 8, "large");
-    Lobby testLobby9 = new Lobby("test9", 29, "large");
-    Lobby testLobby10 = new Lobby("test10", 20, "large");
-    Lobby testLobby11 = new Lobby("test11", 24, "large");
     ServerLobbyList lobbyList = new ServerLobbyList();
+    Lobby testLobby1 = new Lobby("test1", 1, "mid");
     lobbyList.addLobby(testLobby1);
+    Lobby testLobby2 = new Lobby("test2", 2, "large");
     lobbyList.addLobby(testLobby2);
+    Lobby testLobby3 = new Lobby("test3", 3, "large");
     lobbyList.addLobby(testLobby3);
+    Lobby testLobby4 = new Lobby("test4", 4, "large");
     lobbyList.addLobby(testLobby4);
+    Lobby testLobby5 = new Lobby("test5", 5, "large");
     lobbyList.addLobby(testLobby5);
-    lobbyList.addLobby(testLobby6);
-    lobbyList.addLobby(testLobby7);
-    lobbyList.addLobby(testLobby8);
-    lobbyList.addLobby(testLobby9);
-    lobbyList.addLobby(testLobby10);
-    lobbyList.addLobby(testLobby11);
-    Assert.assertEquals("10║test1║0║test2║0║test3║0║test4║0║test5║0║test6║0║test7║0║test8║0║test9║0║test10║0", lobbyList.getTopTen());
+    Assert.assertEquals(
+        "5║test1║0║test2║0║test3║0║test4║0║test5║0",
+        lobbyList.getTopTen());
   }
 
   @Test
@@ -154,4 +145,5 @@ public class TestServerLobbyList {
     s.append(testLobby1.toString()).append("║");
     Assert.assertTrue(s.toString().equals(lobbyList.getLobbiesInGame()));
   }
+
 }
